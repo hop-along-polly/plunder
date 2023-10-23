5ecc5d6432ddd64bffba061d092d5c793830604c
Derek Drummond <derek.ac.drummond@gmail.com>
2023-10-22 23:06:02 -0600

    Add Start Game Modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-const startGame = () => {
-  console.log('Yo ho Yo Ho a pirates life for me!');
+const playNow = () => {
+  console.log('Yo ho yo ho a pirates life for me');
+  $('#play-game-modal').css('display', 'block');
 };
 
-$('#start-game button').on('click', startGame);
+const startGame = () => {
+  console.log("Ahoy mateys were off!")
+};
+
+const closeModal = (event) => {
+  const closeTarget = $(event.target).data('close');
+  const modalToClose = $(`#${closeTarget}`);
+  modalToClose.css('display', 'none');
+}
+
+$('#play-now-btn').on('click', playNow);
+$('#start-game-btn').on('click', startGame);
+$('#play-game-modal').delegate('.modal-close', 'click', closeModal);
