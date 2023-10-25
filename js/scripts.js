3a7bab915b10a0061e2eabc0b38a710c32f2054c
Derek Drummond <derek.ac.drummond@gmail.com>
2023-10-25 09:08:21 -0600

    Add logic for starting game and loading game board

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,14 @@ const playNow = () => {
 };
 
 const startGame = () => {
-  console.log("Ahoy mateys were off!")
+  console.log("Ahoy mateys were off!");
+  const numPlayers = localStorage.getItem('num-players');
+  if (numPlayers < 3) {
+    //load small board
+  } else {
+    // load large board
+    document.location.href = './large-board.html';
+  }
 };
 
 const closeModal = (event) => {
@@ -13,6 +20,16 @@ const closeModal = (event) => {
   modalToClose.css('display', 'none');
 }
 
+const saveNumPlayers = (e) => {
+  const target = $(e.target)
+  localStorage.setItem('num-players', target.data('num-players'));
+  
+  // clear the previous selection before highlighting the new selection
+  target.siblings('.selected').removeClass('selected');
+  target.addClass('selected');
+}
+
 $('#play-now-btn').on('click', playNow);
 $('#start-game-btn').on('click', startGame);
 $('#play-game-modal').delegate('.modal-close', 'click', closeModal);
+$('#play-game-modal').delegate('#num-players img', 'click', saveNumPlayers);
