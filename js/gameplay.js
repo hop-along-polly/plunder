ebcd5bb845bae3e52cae0f6a0aa9a535e22bb002
Derek Drummond <derek.ac.drummond@gmail.com>
2026-09-22 11:33:18 -0600

    Add under construction modal

--- js/gameplay.js
+++ js/gameplay.js
@@ -5,3 +5,15 @@ const rollD6 = () => {
 const rollStormDie = () => {};
 
 const rollMovementDie = () => {};
+
+const closeModal = (event) => {
+  const closeTarget = $(event.target).data('close');
+  $(`#${closeTarget}`).css('display', 'none');
+};
+
+const backToPort = () => {
+  document.location.href = './index.html';
+};
+
+$('#under-construction-modal').delegate('.modal-close', 'click', closeModal);
+$('#back-to-port-btn').on('click', backToPort);
